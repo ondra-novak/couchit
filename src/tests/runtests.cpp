a3d4ec889f7a2ebedf0acb5e4f6b21d3ab9b3ca0
Ondřej Novák <ondra-novak@email.cz>
2017-03-20 16:36:21 +0100

    fix qserver.ini config generation

--- src/tests/runtests.cpp
+++ src/tests/runtests.cpp
@@ -1,6 +1,11 @@
 #include <imtjson/stringview.h>
+#include <string.h>
 #include <fstream>
-
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
 #include "testClass.h"
 
@@ -32,9 +37,25 @@ int main(int argc, char *argv[]) {
 
 	} else {
 
+		char cwd[1000];
+
+		const char *srvpath = argv[0];
+		if (srvpath[0] != '/') {
+#ifdef _WIN32
+			GetCurrentDirectoryA(1000,cwd);
+			strcat_s(cwd,"\\");
+			strcat_s(cwd,srvpath);
+#else
+			getcwd(cwd,1000);
+			strcat(cwd,"/");
+			strcat(cwd,srvpath);
+#endif
+			srvpath = cwd;
+
+		}
 		std::ofstream cfg("qserver.ini", std::ios::out|std::ios::trunc);
 		cfg << "[query_servers]"<<std::endl;
-		cfg << lang << '=' <<argv[0] << " qserver" << std::endl;
+		cfg << lang << '=' <<srvpath << " qserver" << std::endl;
 
 		couchit::runTestLocalview(tst);
 		couchit::runMiniHttpTests(tst);
